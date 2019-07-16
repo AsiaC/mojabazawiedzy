1f3c7294042feef92d9a0c00c2a8917e47bb5c43
AsiaC <czaplicka.asia@gmail.com>
2019-07-16 22:32:43 +0200

    DyrektywyPreprocesora

--- DyrektywyPreprocesora/Program.cs
+++ DyrektywyPreprocesora/Program.cs
@@ -20,6 +20,29 @@ namespace DyrektywyPreprocesora
             //[Conditional("DEBUG")] Atrybut oznaczony atrybutem Conditional zostanie skompilowany tylko wtedy, gdy obecny będzie Określony symbol preprocesora
             //tego warto używac przed klasami żeby nie kompilować nie potrzebnie
 
+            //#pragma
+            //When building an application, you sometimes willingly write some code that triggers a warning.You don’t want to change the code, but you do want to hide the warning. You can do this by using the
+            //#pragma warning directive. (disabling and enabling all warnings)
+
+            //#pragma warning disable
+            //                while (false)
+            //                {
+            //                    Console.WriteLine(“Unreachable code”);
+            //                }
+            //#pragma warning restore
+
+            //You can also choose to disable or restore specific warnings, as shown.The compiler won’t report a warning for the int i statement, but it will report a warning for the unreachable code.You can find the specific error codes in your Output Window in Visual Studio.
+            //Disabling and enabling specific warnings
+            
+            //#pragma warning disable 0162, 0168
+            //int i;
+            //#pragma warning restore 0162
+            //            while (false)
+            //            {
+            //                Console.WriteLine(“Unreachable code”);
+            //            }
+            //#pragma warning restore
+                       
             Console.ReadKey();
         }
         private static void TimerCallback(Object o)
